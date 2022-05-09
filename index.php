9284438ec77cf83aa1a37e5071525b12a15f3f78
Kęstutis Skrebė <kestutis.julius@gmail.com>
2022-05-09 06:17:10 +0300

    Initial commit PHP

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+echo 'Hello <br>';
+//Return the length of the string "Hello world!"
+echo strlen("Hello world!");
+//The PHP str_word_count() function counts the number of words in a string.
+echo str_word_count("Hello world!");
+//The PHP strrev() function reverses a string.
+echo strrev("Hello world!");
+//strpos() - Search For a Text Within a String
+echo strpos("Hello world!", "world");
+//The PHP str_replace() function replaces some characters with some other characters in a string.
+echo str_replace("world", "Dolly", "Hello world!");
+/*
+    switch (n) {
+  case label1:
+    code to be executed if n=label1;
+    break;
+  case label2:
+    code to be executed if n=label2;
+    break;
+  case label3:
+    code to be executed if n=label3;
+    break;
+    ...
+  default:
+    code to be executed if n is different from all labels;
+}
+*/
